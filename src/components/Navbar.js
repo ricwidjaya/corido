71543a9bbe11695644e86f3f1c22fc2c300b660c
Richard Widjaya <richardwidjaya@Richards-MacBook-Air.local>
2023-04-13 18:59:26 +0900

    file cleaning and add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,71 @@
-import React, { useState } from "react";
-import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import github from '../img/github-icon.svg'
+import logo from '../img/logo.svg'
 
 const Navbar = () => {
-  const [isActive, setIsActive] = useState(false);
+  const [isActive, setIsActive] = useState(false)
 
   return (
     <nav
       className="navbar is-transparent"
       role="navigation"
-      aria-label="main-navigation"
-    >
+      aria-label="main-navigation">
       <div className="container">
         <div className="navbar-brand">
           <Link to="/" className="navbar-item" title="Logo">
-            <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+            <img src={logo} alt="corido" style={{ width: '88px' }} />
           </Link>
           {/* Hamburger menu */}
           <button
-            className={`navbar-burger burger ${isActive && "is-active"}`}
+            className={`navbar-burger burger ${isActive && 'is-active'}`}
             aria-expanded={isActive}
-            onClick={() => setIsActive(!isActive)}
-          >
+            onClick={() => setIsActive(!isActive)}>
             <span />
             <span />
             <span />
           </button>
         </div>
-        <ul id="navMenu" className={` navbar-start has-text-centered navbar-menu ${isActive && "is-active"}`}>
-            {/* TODO: inline override of padding is a result of refactoring
+        <ul
+          id="navMenu"
+          className={` navbar-start has-text-centered navbar-menu ${
+            isActive && 'is-active'
+          }`}>
+          {/* TODO: inline override of padding is a result of refactoring
                 to a ul for accessibilty purposes, would like to see a css
                 re-write that makes this unneccesary.
              */}
-            <li className="navbar-item" style={{padding: "0px"}}>
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
+          <li className="navbar-item" style={{ padding: '0px' }}>
+            <Link className="navbar-item" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="navbar-item" style={{ padding: '0px' }}>
             <Link className="navbar-item" to="/products">
               Products
             </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
+          </li>
+          <li className="navbar-item" style={{ padding: '0px' }}>
             <Link className="navbar-item" to="/blog">
               Blog
             </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
+          </li>
+          <li className="navbar-item" style={{ padding: '0px' }}>
             <Link className="navbar-item" to="/contact">
               Contact
             </Link>
-            </li>
-            <li className="navbar-item" style={{padding: "0px"}}>
+          </li>
+          <li className="navbar-item" style={{ padding: '0px' }}>
             <Link className="navbar-item" to="/contact/examples">
               Form Examples
             </Link>
-            </li>
+          </li>
           <li className="navbar-end has-text-centered">
             <a
               className="navbar-item"
               href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
               target="_blank"
-              rel="noopener noreferrer"
-            >
+              rel="noopener noreferrer">
               <span className="icon">
                 <img src={github} alt="Github" />
               </span>
@@ -73,7 +74,7 @@ const Navbar = () => {
         </ul>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
