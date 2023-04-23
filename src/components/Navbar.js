14c2324441716f991d47219d22bbfbeacaf00195
Richard Widjaya <richard.widjaya@gmail.com>
2023-04-23 18:17:33 +0900

    cleaning up navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => {
@@ -32,8 +31,8 @@ const Navbar = () => {
             isActive && 'is-active'
           }`}>
           {/* TODO: inline override of padding is a result of refactoring
-                to a ul for accessibilty purposes, would like to see a css
-                re-write that makes this unneccesary.
+                to a ul for accessibility purposes, would like to see a css
+                re-write that makes this unneccessary.
              */}
           <li className="navbar-item" style={{ padding: '0px' }}>
             <Link className="navbar-item" to="/about">
@@ -41,35 +40,9 @@ const Navbar = () => {
             </Link>
           </li>
           <li className="navbar-item" style={{ padding: '0px' }}>
-            <Link className="navbar-item" to="/products">
-              Products
-            </Link>
-          </li>
-          <li className="navbar-item" style={{ padding: '0px' }}>
             <Link className="navbar-item" to="/blog">
               Blog
             </Link>
-          </li>
-          <li className="navbar-item" style={{ padding: '0px' }}>
-            <Link className="navbar-item" to="/contact">
-              Contact
-            </Link>
-          </li>
-          <li className="navbar-item" style={{ padding: '0px' }}>
-            <Link className="navbar-item" to="/contact/examples">
-              Form Examples
-            </Link>
-          </li>
-          <li className="navbar-end has-text-centered">
-            <a
-              className="navbar-item"
-              href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-              target="_blank"
-              rel="noopener noreferrer">
-              <span className="icon">
-                <img src={github} alt="Github" />
-              </span>
-            </a>
           </li>
         </ul>
       </div>
